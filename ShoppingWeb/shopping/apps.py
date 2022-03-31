246867d8ba7ddd68e5ea0ae35987d16ebb46d45f
Aaron S <acs4631@gmail.com>
2022-03-31 15:53:03 -0400

    Refactor + Update
    Code renames, code additions, html page links, etc

--- ShoppingWeb/shopping/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-##########################################
-#   CONFIG CLASS
-##########################################
-# defines the application name as shopping, same as folder and in urls.py reference
-class ShoppingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shopping'
